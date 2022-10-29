e4ff5b71a2d9aef6a1131636675f97f8b8783e0e
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-29 15:11:29 +0700

    Birthday Card - Add Birthday Greeting Image with it's preview

--- app/src/main/java/io/github/fatimazza/mycomposeapp/BirthdayCardActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/BirthdayCardActivity.kt
@@ -3,6 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
@@ -45,10 +47,17 @@ fun BirthdayGreetingWithText(message: String, from: String){
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String){
+    val image = painterResource(id = R.drawable.androidparty)
+    Image(painter = image, contentDescription = null)
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     MyComposeAppTheme {
         BirthdayGreetingWithText(message = "Happy birthday, Za!", " - from Fatima")
+        BirthdayGreetingWithImage(message = "Happy birthday, Za!", " - from Fatima")
     }
 }
