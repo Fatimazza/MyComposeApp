b5fed9e9933f6ce5a80d879e3a5376df1b0a2652
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-29 14:38:22 +0700

    Birthday Card - Change the text Font Size

--- app/src/main/java/io/github/fatimazza/mycomposeapp/BirthdayCardActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/BirthdayCardActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class BirthdayCardActivity : ComponentActivity() {
@@ -32,7 +33,8 @@ class BirthdayCardActivity : ComponentActivity() {
 @Composable
 fun BirthdayGreetingWithText(message: String){
     Text(
-        text = message
+        text = message,
+        fontSize = 36.sp
     )
 }
 
