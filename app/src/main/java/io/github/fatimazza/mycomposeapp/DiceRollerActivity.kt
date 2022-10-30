c9299ed55d5c5c848ae6f484b3751541a4c1b3bb
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 08:29:45 +0700

    Dice Roller App - Create Vertical Layout using Column

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DiceRollerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DiceRollerActivity.kt
@@ -3,6 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
@@ -32,7 +33,10 @@ class DiceRollerActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
-
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {}
 }
 
 @Preview(showBackground = true)
