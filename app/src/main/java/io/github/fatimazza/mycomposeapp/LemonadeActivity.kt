7edb3bb182b598fde5bee02188632c169757435f
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 10:40:09 +0700

    Lemonade App - Make a vertical Layout with Text on the center using Column

--- app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
@@ -3,12 +3,16 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -30,7 +34,13 @@ fun LemonadeApp() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Text(text = "This is Lemonade App")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(stringResource(R.string.lemon_tree))
+        }
     }
 }
 
