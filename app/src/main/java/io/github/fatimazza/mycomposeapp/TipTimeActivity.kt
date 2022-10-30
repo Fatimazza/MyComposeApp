7666111d3a938a05f86af5c4eafdb71e7b2159d5
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 21:42:21 +0700

    Tip Time App - Add a Function to show a Text Field

--- app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,7 +46,16 @@ fun TipTimeScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(16.dp))
+        EditNumberField()
     }
+}
+
+@Composable
+fun EditNumberField(){
+    TextField(
+        value = "",
+        onValueChange = {}
+    )
 }
 
 @Preview(showBackground = true)
