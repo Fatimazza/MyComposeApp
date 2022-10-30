71de3dbb75df388105fbf1345fd8b3b09d78adc9
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 08:32:21 +0700

    Dice Roller App - Add a Button with title

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DiceRollerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DiceRollerActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -36,7 +39,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.dice_roller_roll))
+        }
+    }
 }
 
 @Preview(showBackground = true)
