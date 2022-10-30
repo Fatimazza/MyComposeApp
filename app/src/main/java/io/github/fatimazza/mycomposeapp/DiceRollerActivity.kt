93c2110747917d03f4a5989d9f2be1b132481d51
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 08:41:39 +0700

    Dice Roller App - Show the first dice Image

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DiceRollerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DiceRollerActivity.kt
@@ -3,6 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -13,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
@@ -40,6 +42,10 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1"
+        )
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.dice_roller_roll))
         }
