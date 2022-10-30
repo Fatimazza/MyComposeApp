6353d21fce557bab76be7b3f786070b5ae3b51c2
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 10:48:12 +0700

    Lemonade App - Add a Space below the Text

--- app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
@@ -3,9 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class LemonadeActivity : ComponentActivity() {
@@ -40,6 +39,7 @@ fun LemonadeApp() {
             modifier = Modifier.fillMaxSize()
         ) {
             Text(stringResource(R.string.lemon_tree))
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
