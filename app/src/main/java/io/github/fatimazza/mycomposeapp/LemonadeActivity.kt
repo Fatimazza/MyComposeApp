5e6116b507cd6b79bd95435cfe2edb82fced8fc8
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 09:51:14 +0700

    Lemonade App - Call the Lemonade function to be displayed on device

--- app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
@@ -3,11 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -16,13 +12,7 @@ class LemonadeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
+                LemonadeApp()
             }
         }
     }
