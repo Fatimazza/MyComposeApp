e2428280338656c13afa4f66ba26d54790287d43
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 21:36:27 +0700

    Tip Time App - Make a vertical Layout using Column

--- app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
@@ -3,12 +3,16 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class TipTimeActivity : ComponentActivity() {
@@ -29,8 +33,13 @@ class TipTimeActivity : ComponentActivity() {
 }
 
 @Composable
-fun TipTimeScreen(){
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
 
+    }
 }
 
 @Preview(showBackground = true)
