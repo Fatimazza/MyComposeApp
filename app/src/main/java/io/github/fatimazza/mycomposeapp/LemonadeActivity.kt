c479c0c7453ecd7349f21eaf1781197bed31e7de
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 10:49:33 +0700

    Lemonade App - Add a wrapped Image from the resource

--- app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
@@ -3,6 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,6 +42,13 @@ fun LemonadeApp() {
         ) {
             Text(stringResource(R.string.lemon_tree))
             Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(R.drawable.lemon_tree),
+                contentDescription = stringResource(
+                    R.string.lemon_content_description),
+                modifier = Modifier
+                    .wrapContentSize()
+            )
         }
     }
 }
