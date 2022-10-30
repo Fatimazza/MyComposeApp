73d47eca1ccbe129c8f0637b899d6229bbae50e4
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 10:55:12 +0700

    Lemonade App - Add Image border and padding

--- app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
@@ -3,14 +3,18 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,6 +52,11 @@ fun LemonadeApp() {
                     R.string.lemon_content_description),
                 modifier = Modifier
                     .wrapContentSize()
+                    .border(
+                        BorderStroke(2.dp, Color(105, 205, 216)),
+                        shape = RoundedCornerShape(4.dp)
+                    )
+                    .padding(16.dp)
             )
         }
     }
