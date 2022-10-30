7a15b46fa63f4ac24e18ed59afea6371c836a6b3
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-30 09:49:28 +0700

    Lemonade App -  Change the Preview Composable function

--- app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/LemonadeActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,8 +34,8 @@ fun LemonadeApp() {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview2() {
+fun LemonadePreview() {
     MyComposeAppTheme {
-
+        LemonadeApp()
     }
 }
