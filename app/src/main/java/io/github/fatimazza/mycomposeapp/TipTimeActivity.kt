632444556a3bc4437a8c2c1363e852c400f4cc8d
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-10-31 12:26:56 +0700

    Tip Time App - Add Text to display Tip amount

--- app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,6 +51,13 @@ fun TipTimeScreen() {
         )
         Spacer(Modifier.height(16.dp))
         EditNumberField()
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = stringResource(R.string.tip_amount, ""),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
