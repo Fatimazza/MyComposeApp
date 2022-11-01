73cb1b5ca041df3a2b1164fe84d9d2d07db21da1
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-01 14:01:40 +0700

    Tip Time App v2 - Change a Next action button on Keyboard

--- app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/TipTimeActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -92,7 +93,9 @@ fun EditNumberField(
         label = { Text(stringResource(label)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next)
     )
 }
 
