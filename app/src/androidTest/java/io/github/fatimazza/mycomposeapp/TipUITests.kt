ef47b6d17338081f002714bc628621b3e4515ab5
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-02 11:39:21 +0700

    Tip Time App Testing - Add an Instrumentation Test

--- app/src/androidTest/java/io/github/fatimazza/mycomposeapp/TipUITests.kt
+++ app/src/androidTest/java/io/github/fatimazza/mycomposeapp/TipUITests.kt
@@ -1,4 +1,29 @@
 package io.github.fatimazza.mycomposeapp
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
+import org.junit.Rule
+import org.junit.Test
+
 class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            MyComposeAppTheme {
+                TipTimeScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)")
+            .performTextInput("20")
+        composeTestRule.onNodeWithText("Tip amount $2.00").assertExists()
+    }
 }
