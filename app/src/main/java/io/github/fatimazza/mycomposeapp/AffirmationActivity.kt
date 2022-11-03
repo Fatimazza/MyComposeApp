484dd0c666e7ca51371da37db499f967efe7ae56
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 12:38:14 +0700

    Affirmation App - Add a Vertical Layout consists of Image and Text

--- app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
@@ -3,11 +3,16 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.model.Affirmation
@@ -36,7 +41,15 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
         modifier = modifier.padding(8.dp),
         elevation = 4.dp
     ) {
-
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId)
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId)
+            )
+        }
     }
 }
 
