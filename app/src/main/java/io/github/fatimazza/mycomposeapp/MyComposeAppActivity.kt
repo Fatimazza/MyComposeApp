c70036e6b7219c84dcaa573662eb73a99cd076a7
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 14:20:30 +0700

    My Compose App - Add a Text and Buttons

--- app/src/main/java/io/github/fatimazza/mycomposeapp/MyComposeAppActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/MyComposeAppActivity.kt
@@ -3,13 +3,16 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class MyComposeAppActivity : ComponentActivity() {
@@ -24,7 +27,47 @@ class MyComposeAppActivity : ComponentActivity() {
 @Composable
 fun MyComposeApp() {
     MyComposeAppTheme {
-        
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp, 16.dp),
+        ) {
+            Text(
+                text = stringResource(R.string.app_name),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.h2
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.title_birthday_card))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.title_lemonade))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.title_dice_roller))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.title_tip_time))
+            }
+        }
     }
 }
 
