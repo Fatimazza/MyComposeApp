d169584e02bcd9f23672a76a6701c2ba4b76144f
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 12:18:00 +0700

    Affirmation App - Create an Datasource Class under the Data package

---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/data/Datasource.kt
@@ -0,0 +1,20 @@
+package io.github.fatimazza.mycomposeapp.data
+
+import io.github.fatimazza.mycomposeapp.R
+import io.github.fatimazza.mycomposeapp.model.Affirmation
+
+class Datasource(){
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.affirmation_img1),
+            Affirmation(R.string.affirmation2, R.drawable.affirmation_img2),
+            Affirmation(R.string.affirmation3, R.drawable.affirmation_img3),
+            Affirmation(R.string.affirmation4, R.drawable.affirmation_img4),
+            Affirmation(R.string.affirmation5, R.drawable.affirmation_img5),
+            Affirmation(R.string.affirmation6, R.drawable.affirmation_img6),
+            Affirmation(R.string.affirmation7, R.drawable.affirmation_img7),
+            Affirmation(R.string.affirmation8, R.drawable.affirmation_img8),
+            Affirmation(R.string.affirmation9, R.drawable.affirmation_img9),
+            Affirmation(R.string.affirmation10, R.drawable.affirmation_img10))
+    }
+}
