5b55c8638afcec8dcbcf2372f66b080bc39066e8
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 12:34:58 +0700

    Affirmation App - Add a Card Layout

--- app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
@@ -3,9 +3,14 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.fatimazza.mycomposeapp.model.Affirmation
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class AffirmationActivity : ComponentActivity() {
@@ -26,8 +31,13 @@ fun AffirmationApp() {
 }
 
 @Composable
-fun AffirmationCard() {
-    
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
