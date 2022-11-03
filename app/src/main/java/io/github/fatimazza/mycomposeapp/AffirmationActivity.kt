22c507fb415643fa93ca06fa65982694942f0c21
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 12:57:05 +0700

    Affirmation App - Show the Affirmation List data from the Datasource class

--- app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
@@ -12,17 +12,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.github.fatimazza.mycomposeapp.data.Datasource
 import io.github.fatimazza.mycomposeapp.model.Affirmation
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
+import javax.sql.DataSource
 
 class AffirmationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +37,12 @@ class AffirmationActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationApp() {
-    val context = LocalContext.current
-
     MyComposeAppTheme {
+        Scaffold(
+            content = {
+                AffirmationLists(affirmationList = Datasource().loadAffirmations())
+            }
+        )
     }
 }
 
