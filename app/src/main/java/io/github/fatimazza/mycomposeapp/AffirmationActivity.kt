2512582c2b4be4c146252fd50402c7e2545d31be
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 12:27:05 +0700

    Affirmation App - Remove unused Imports

--- app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
@@ -3,11 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
