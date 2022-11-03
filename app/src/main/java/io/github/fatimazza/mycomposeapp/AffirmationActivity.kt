f25144219ec9cb89a6dd6b5705ab15dbb087ef94
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-03 12:25:00 +0700

    Affirmation App - Add Affirmation App Composable Function

--- app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/AffirmationActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,15 +15,14 @@ class AffirmationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                }
-            }
+            AffirmationApp()
         }
+    }
+}
+
+@Composable
+fun AffirmationApp() {
+    MyComposeAppTheme {
     }
 }
 
