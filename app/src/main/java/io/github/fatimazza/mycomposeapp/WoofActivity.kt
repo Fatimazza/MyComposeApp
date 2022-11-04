de1f038e55126da0900d3cc0361ee5f20ca13e36
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-04 15:13:35 +0700

    Woof App - Add two Texts to show name and age in Dog Information

--- app/src/main/java/io/github/fatimazza/mycomposeapp/WoofActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/WoofActivity.kt
@@ -3,7 +3,12 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.WoofTheme
 
@@ -31,14 +36,25 @@ fun WoofApp() {
  * @param modifier modifiers to set to this composable
  */
 @Composable
-fun DogInformation() {
-    
+fun DogInformation(
+    @StringRes dogName: Int,
+    @StringRes dogAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Text(
+            text = stringResource(dogName)
+        )
+        Text(
+            text = stringResource(R.string.woof_years_old, dogAge)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun WoofPreview() {
     WoofTheme(darkTheme = false) {
-        WoofApp()
+        DogInformation(R.string.woof_dog_name_1,0)
     }
 }
