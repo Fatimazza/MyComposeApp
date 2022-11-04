b3167607a4ec972cc20d6f96bf660bf8daac6d0a
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-11-04 14:38:24 +0700

    Woof App - Delete unused Composable Function

--- app/src/main/java/io/github/fatimazza/mycomposeapp/WoofActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/WoofActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,22 +21,16 @@ class WoofActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting2("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     MyComposeAppTheme {
-        Greeting2("Android")
     }
 }
