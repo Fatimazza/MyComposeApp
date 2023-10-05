5a9ac313b31c58661ee409ad9dbd945f75ad709c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-05 17:07:11 +0700

    Dessert Clicker App - Add custom App Bar as Top Bar

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
@@ -3,19 +3,27 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class DessertClickerActivity : ComponentActivity() {
@@ -35,11 +43,44 @@ class DessertClickerActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DesertClickerApp() {
-    Scaffold(topBar = { TopAppBar(title = { Text(stringResource(id = R.string.title_dessert_clicker)) }) }) { contentPadding ->
+    Scaffold(topBar = {
+        DessertClickerAppBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+        )
+    }) { contentPadding ->
         Column(modifier = Modifier.padding(contentPadding)) {}
+    }
+}
+
+@Composable
+private fun DessertClickerAppBar(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier.padding(start = 16.dp),
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.titleLarge,
+        )
+        IconButton(
+            onClick = { },
+            modifier = Modifier.padding(end = 16.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+                contentDescription = stringResource(R.string.dessert_share),
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+        }
     }
 }
 
