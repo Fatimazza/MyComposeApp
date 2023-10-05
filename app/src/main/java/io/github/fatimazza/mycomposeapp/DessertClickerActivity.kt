709969cf9dbf14460d65a10deeb786b426184f30
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-05 16:59:57 +0700

    Dessert Clicker App - Add Scaffold with Top Bar

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
@@ -3,11 +3,18 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -28,9 +35,12 @@ class DessertClickerActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DesertClickerApp() {
-
+    Scaffold(topBar = { TopAppBar(title = { Text(stringResource(id = R.string.title_dessert_clicker)) }) }) { contentPadding ->
+        Column(modifier = Modifier.padding(contentPadding)) {}
+    }
 }
 
 @Preview(showBackground = true)
