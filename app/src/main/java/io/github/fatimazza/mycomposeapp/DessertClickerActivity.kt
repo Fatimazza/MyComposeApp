b1727b88298c38f03596412f8de68fb55dc6b395
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-05 16:38:56 +0700

    Dessert Clicker App - Create Composable Function and Preview

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +21,7 @@ class DessertClickerActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+                    DesertClickerApp()
                 }
             }
         }
@@ -30,17 +29,14 @@ class DessertClickerActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DesertClickerApp() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DesertClickerAppPreview() {
     MyComposeAppTheme {
-        Greeting2("Android")
+        DesertClickerApp()
     }
 }
