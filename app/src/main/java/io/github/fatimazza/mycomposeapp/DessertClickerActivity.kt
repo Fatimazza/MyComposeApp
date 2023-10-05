1232b2c60e3a11862b664bb92bf32bdf883aa057
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-05 17:28:24 +0700

    Dessert Clicker App - Add specific Dessert Image in main screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
@@ -7,12 +7,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Icon
@@ -26,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -105,6 +110,24 @@ fun DessertClickerScreen(
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
+        Column {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.dessert_froyo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(dimensionResource(R.dimen.dessert_image_size))
+                        .height(dimensionResource(R.dimen.dessert_image_size))
+                        .align(Alignment.Center)
+                        .clickable { },
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
     }
 }
 
