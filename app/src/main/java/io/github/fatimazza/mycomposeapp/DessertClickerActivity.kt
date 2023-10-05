720fcaf4d0c5d53688832d2368239b561a428d87
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-05 17:24:32 +0700

    Dessert Clicker App - Add main screen with full size Image

--- app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/DessertClickerActivity.kt
@@ -5,9 +5,10 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,7 +24,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,7 +60,9 @@ fun DesertClickerApp() {
                 .background(MaterialTheme.colorScheme.primary)
         )
     }) { contentPadding ->
-        Column(modifier = Modifier.padding(contentPadding)) {}
+        DessertClickerScreen(
+            modifier = Modifier.padding(contentPadding)
+        )
     }
 }
 
@@ -87,6 +92,19 @@ private fun DessertClickerAppBar(
                 tint = MaterialTheme.colorScheme.onPrimary
             )
         }
+    }
+}
+
+@Composable
+fun DessertClickerScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.dessert_bakery_back),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
