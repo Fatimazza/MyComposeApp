82a2dd39985caae0e21c4249d0a4e81310785c94
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-05 10:44:22 +0700

    Woof App v4 - Add new Font family

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/theme/Type.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/theme/Type.kt
@@ -2,9 +2,21 @@ package io.github.fatimazza.mycomposeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.github.fatimazza.mycomposeapp.R
+
+
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
