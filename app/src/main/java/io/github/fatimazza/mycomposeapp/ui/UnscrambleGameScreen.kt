cd956695c9d36db3deb5e781512d15ce90617a22
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-06 12:43:22 +0700

    Unscramble App - Add Game Layout with Card containing Text

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
@@ -2,9 +2,13 @@ package io.github.fatimazza.mycomposeapp.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -31,6 +36,34 @@ fun UnscrambleGameScreen() {
             text = stringResource(R.string.title_unscramble),
             style = typography.titleLarge,
         )
+        GameLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(mediumPadding)
+        )
+    }
+}
+
+@Composable
+fun GameLayout(
+    modifier: Modifier = Modifier
+) {
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
+
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(mediumPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(mediumPadding)
+        ) {
+            Text(
+                text = stringResource(id = R.string.unscramble_word_count, 0),
+            )
+        }
     }
 }
 
