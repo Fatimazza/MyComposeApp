7cbd23f93eb90f3a98fbe9459f93062c034c3128
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-06 12:59:08 +0700

    Unscramble App - Add Input Word Text Field on Card of Game Layout

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
@@ -7,19 +7,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -83,6 +88,26 @@ fun GameLayout(
                 text = stringResource(R.string.unscramble_instructions),
                 textAlign = TextAlign.Center,
                 style = typography.titleMedium
+            )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                shape = shapes.large,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = colorScheme.surface,
+                    unfocusedContainerColor = colorScheme.surface,
+                    disabledContainerColor = colorScheme.surface,
+                ),
+                onValueChange = { },
+                label = { Text(stringResource(R.string.unscramble_enter_your_word)) },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { }
+                )
             )
         }
     }
