ea0f9bc81fe4806dca62ed8a6e06c6b4e068b88a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-06 12:48:45 +0700

    Unscramble App - Add more Texts on Card of Game Layout

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
@@ -1,5 +1,6 @@
 package io.github.fatimazza.mycomposeapp.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,13 +10,17 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.R
@@ -61,7 +66,23 @@ fun GameLayout(
             modifier = Modifier.padding(mediumPadding)
         ) {
             Text(
+                modifier = Modifier
+                    .clip(shapes.medium)
+                    .background(colorScheme.surfaceTint)
+                    .padding(horizontal = 10.dp, vertical = 4.dp)
+                    .align(alignment = Alignment.End),
                 text = stringResource(id = R.string.unscramble_word_count, 0),
+                style = typography.titleMedium,
+                color = colorScheme.onPrimary
+            )
+            Text(
+                text = "scrambleun",
+                style = typography.displayMedium
+            )
+            Text(
+                text = stringResource(R.string.unscramble_instructions),
+                textAlign = TextAlign.Center,
+                style = typography.titleMedium
             )
         }
     }
