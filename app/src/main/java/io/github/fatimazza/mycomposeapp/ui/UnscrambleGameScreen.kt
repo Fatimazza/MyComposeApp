47b36b829cf497b2d6bb548e8c3bba3327525a4c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-06 12:28:50 +0700

    Unscramble App - Add empty Game Screen to replace unused Composable function

---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
@@ -0,0 +1,17 @@
+package io.github.fatimazza.mycomposeapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
+
+@Composable
+fun UnscrambleGameScreen() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    MyComposeAppTheme {
+        UnscrambleGameScreen()
+    }
+}
