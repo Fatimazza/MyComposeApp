9aef83c6ee773517406de6e0cc47e4286e1fe509
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-06 12:34:24 +0700

    Unscramble App - Add initial layout with Text on Game Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/UnscrambleGameScreen.kt
@@ -1,11 +1,37 @@
 package io.github.fatimazza.mycomposeapp.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 @Composable
 fun UnscrambleGameScreen() {
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(mediumPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.title_unscramble),
+            style = typography.titleLarge,
+        )
+    }
 }
 
 @Preview(showBackground = true)
