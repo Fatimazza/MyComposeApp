70363ac22ec7278c9e4786d8ad92fec69ee83936
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 04:06:57 +0700

    Cupcake App - Add View Model

---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderViewModel.kt
@@ -0,0 +1,11 @@
+package io.github.fatimazza.mycomposeapp.ui.cupcake
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * [OrderViewModel] holds information about a cupcake order in terms of quantity, flavor, and
+ * pickup date. It also knows how to calculate the total price based on these order details.
+ */
+class OrderViewModel : ViewModel() {
+
+}
