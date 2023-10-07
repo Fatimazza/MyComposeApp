35c7d35dcaff91b1e493f4e960c30b890519897f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 05:30:48 +0700

    Cupcake App - Make dynamic multiple Radio Button in Select Option Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/SelectOptionScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/SelectOptionScreen.kt
@@ -5,10 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Divider
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -23,22 +28,41 @@ import io.github.fatimazza.mycomposeapp.R
  */
 @Composable
 fun SelectOptionScreen(
+    options: List<String>,
+    onSelectionChanged: (String) -> Unit = {},
     modifier: Modifier
 ) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))) {
-            Row(
-                modifier = Modifier,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                RadioButton(
-                    selected = false,
-                    onClick = {}
-                )
-                Text(text = "Option 1")
+        var selectedValue by rememberSaveable { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier.padding(
+                dimensionResource(R.dimen.padding_medium)
+            )
+        ) {
+            options.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = selectedValue == item,
+                        onClick = {
+                            selectedValue = item
+                            onSelectionChanged(item)
+                        }
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = selectedValue == item,
+                        onClick = {
+                            selectedValue = item
+                            onSelectionChanged(item)
+                        }
+                    )
+                    Text(item)
+                }
             }
             Divider(
                 thickness = dimensionResource(R.dimen.thickness_divider),
@@ -52,6 +76,7 @@ fun SelectOptionScreen(
 @Composable
 fun SelectOptionPreview() {
     SelectOptionScreen(
+        options = listOf("Option 1", "Option 2", "Option 3", "Option 4"),
         modifier = Modifier.fillMaxHeight()
     )
 }
