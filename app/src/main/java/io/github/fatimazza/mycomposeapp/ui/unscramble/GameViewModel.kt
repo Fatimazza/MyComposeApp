805ad83985e8089190d7cc1aa3340c45118adfb9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-07 12:14:22 +0700

    Unscramble App - Add Game UI State in View Model

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/unscramble/GameViewModel.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/unscramble/GameViewModel.kt
@@ -1,7 +1,12 @@
 package io.github.fatimazza.mycomposeapp.ui.unscramble
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel : ViewModel() {
-    
+    // Game UI state
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 }
