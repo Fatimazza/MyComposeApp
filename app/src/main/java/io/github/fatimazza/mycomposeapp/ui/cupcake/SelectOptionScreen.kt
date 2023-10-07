dbdb0e3aa64a3dfa8a2ff0e90671f20eeec51fe2
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 05:24:06 +0700

    Cupcake App - Add Radio Button in Select Option Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/SelectOptionScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/SelectOptionScreen.kt
@@ -1,9 +1,19 @@
 package io.github.fatimazza.mycomposeapp.ui.cupcake
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
 
 /**
  * Composable that displays the list of items as [RadioButton] options,
@@ -15,7 +25,27 @@ import androidx.compose.ui.tooling.preview.Preview
 fun SelectOptionScreen(
     modifier: Modifier
 ) {
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))) {
+            Row(
+                modifier = Modifier,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = false,
+                    onClick = {}
+                )
+                Text(text = "Option 1")
+            }
+            Divider(
+                thickness = dimensionResource(R.dimen.thickness_divider),
+                modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
+            )
+        }
+    }
 }
 
 @Preview
