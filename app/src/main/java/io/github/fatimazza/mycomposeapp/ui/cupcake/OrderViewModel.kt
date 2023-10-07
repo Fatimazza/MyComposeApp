bdaa98ababaa41ac8fd3cc775845ce2708dd15ea
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 04:12:31 +0700

    Cupcake App - Add Pick Up Options function in View Model

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderViewModel.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderViewModel.kt
@@ -5,6 +5,9 @@ import io.github.fatimazza.mycomposeapp.data.cupcake.OrderUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 /**
  * [OrderViewModel] holds information about a cupcake order in terms of quantity, flavor, and
@@ -15,6 +18,21 @@ class OrderViewModel : ViewModel() {
     /**
      * Cupcake state for this order
      */
-    private val _uiState = MutableStateFlow(OrderUiState())
+    private val _uiState = MutableStateFlow(OrderUiState(pickupOptions = pickupOptions()))
     val uiState: StateFlow<OrderUiState> = _uiState.asStateFlow()
+
+    /**
+     * Returns a list of date options starting with the current date and the following 3 dates.
+     */
+    private fun pickupOptions(): List<String> {
+        val dateOptions = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        // add current date and the following 3 dates.
+        repeat(4) {
+            dateOptions.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return dateOptions
+    }
 }
