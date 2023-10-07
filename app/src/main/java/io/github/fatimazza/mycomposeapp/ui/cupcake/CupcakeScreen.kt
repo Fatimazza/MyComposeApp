e9ed238432aba91565a0aca8a44ea75cc24f1953
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 03:29:00 +0700

    Cupcake App - Add initial layout with Text on Cupcake Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/CupcakeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/CupcakeScreen.kt
@@ -1,8 +1,32 @@
 package io.github.fatimazza.mycomposeapp.ui.cupcake
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
+import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 @Composable
 fun CupcakeApp() {
+    Scaffold(
+        topBar = {
+            Text(text = stringResource(id = R.string.title_cupcake))
+        }
+    ) {
+        Column (modifier = Modifier.padding(it)) {
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    MyComposeAppTheme {
+        CupcakeApp()
+    }
 }
