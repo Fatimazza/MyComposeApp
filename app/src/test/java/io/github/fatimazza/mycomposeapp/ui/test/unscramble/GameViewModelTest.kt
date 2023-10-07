90ceec9f781766bac2367a0f3a9e859fa4a58e2b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-07 16:04:59 +0700

    Unscramble App - Add Unit Test for First Load Screen

--- app/src/test/java/io/github/fatimazza/mycomposeapp/ui/test/unscramble/GameViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/mycomposeapp/ui/test/unscramble/GameViewModelTest.kt
@@ -1,4 +1,36 @@
 package io.github.fatimazza.mycomposeapp.ui.test.unscramble
 
+import io.github.fatimazza.mycomposeapp.data.unscramble.getUnscrambledWord
+import io.github.fatimazza.mycomposeapp.ui.unscramble.GameViewModel
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
 class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_Initialization_FirstWordLoaded() {
+        /**
+         *  Warning: This way to retrieve the uiState works because MutableStateFlow is used. In the
+         *  upcoming units you will learn about advanced usages of StateFlow that creates a stream
+         *  of data and you need to react to handle the stream. For those scenarios you will write
+         *  unit tests using different methods/approaches. This applies to all the usages of
+         *  viewModel.uiState.value in this class.
+         **/
+        val gameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
+
+        // Assert that current word is scrambled.
+        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
+        // Assert that current word count is set to 1.
+        assertTrue(gameUiState.currentWordCount == 1)
+        // Assert that initially the score is 0.
+        assertTrue(gameUiState.score == 0)
+        // Assert that wrong word guessed is false.
+        assertFalse(gameUiState.isGuessedWordWrong)
+        // Assert that game is not over.
+        assertFalse(gameUiState.isGameOver)
+    }
 }
