aae088496a6833c76506aa4a07675c4b95c1942b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 03:33:50 +0700

    Cupcake App - Add separated Top App Bar

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/CupcakeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/CupcakeScreen.kt
@@ -2,8 +2,12 @@ package io.github.fatimazza.mycomposeapp.ui.cupcake
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -15,12 +19,29 @@ import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 fun CupcakeApp() {
     Scaffold(
         topBar = {
-            Text(text = stringResource(id = R.string.title_cupcake))
+            CupcakeAppBar()
         }
     ) {
-        Column (modifier = Modifier.padding(it)) {
+        Column(modifier = Modifier.padding(it)) {
         }
     }
+}
+
+/**
+ * Composable that displays the topBar and displays back button if back navigation is possible.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CupcakeAppBar(
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.title_cupcake)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+    )
 }
 
 @Preview(showBackground = true)
