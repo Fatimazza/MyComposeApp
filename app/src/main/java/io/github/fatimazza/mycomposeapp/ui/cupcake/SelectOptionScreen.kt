4741c60938fe41a2520fad0cd3b9defa954dc8bd
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 07:14:32 +0700

    Cupcake App - Add Cancel and Next Button in Select Option Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/SelectOptionScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/SelectOptionScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.components.CupcakeFormattedPriceLabel
@@ -79,6 +83,27 @@ fun SelectOptionScreen(
                         bottom = dimensionResource(R.dimen.padding_medium)
                     )
             )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(1f, false),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            OutlinedButton(modifier = Modifier.weight(1f), onClick = {}) {
+                Text(stringResource(R.string.cupcake_cancel))
+            }
+            Button(
+                modifier = Modifier.weight(1f),
+                // the button is enabled when the user makes a selection
+                enabled = selectedValue.isNotEmpty(),
+                onClick = {}
+            ) {
+                Text(stringResource(R.string.cupcake_next))
+            }
         }
     }
 }
