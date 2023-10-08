d5559d67f02de3af464a3129378d9bdc6f60bf13
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 07:26:03 +0700

    Cupcake App - Texts in Order Summary Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderSummaryScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderSummaryScreen.kt
@@ -1,9 +1,17 @@
 package io.github.fatimazza.mycomposeapp.ui.cupcake
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
 
 /**
  * This composable expects [orderUiState] that represents the order state, [onCancelButtonClicked]
@@ -14,7 +22,19 @@ import androidx.compose.ui.tooling.preview.Preview
 fun OrderSummaryScreen(
     modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            Text("Title")
+            Text(text = "description", fontWeight = FontWeight.Bold)
+            Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+        }
+    }
 }
 
 @Preview
