c7b0929775c0efa057019bd820e1afbf715a9fdd
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 07:28:51 +0700

    Cupcake App - Make dynamic multiple Texts in Order Summary Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderSummaryScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/cupcake/OrderSummaryScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.R
@@ -22,6 +23,16 @@ import io.github.fatimazza.mycomposeapp.R
 fun OrderSummaryScreen(
     modifier: Modifier = Modifier
 ) {
+    //Create a list of order summary to display
+    val items = listOf(
+        // Summary line 1: display selected quantity
+        Pair(stringResource(R.string.cupcake_quantity), "0"),
+        // Summary line 2: display selected flavor
+        Pair(stringResource(R.string.cupcake_flavor), "chocolate"),
+        // Summary line 3: display selected pickup date
+        Pair(stringResource(R.string.cupcake_pickup_date), "01-02-2023")
+    )
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
@@ -30,9 +41,11 @@ fun OrderSummaryScreen(
             modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
         ) {
-            Text("Title")
-            Text(text = "description", fontWeight = FontWeight.Bold)
-            Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+            items.forEach { item ->
+                Text(item.first.uppercase())
+                Text(text = item.second, fontWeight = FontWeight.Bold)
+                Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+            }
         }
     }
 }
