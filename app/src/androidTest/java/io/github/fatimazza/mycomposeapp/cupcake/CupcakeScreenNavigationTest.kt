ea24e1b084968ce31f2f4ee82eabe78f8fa92c98
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-08 11:58:58 +0700

    Cupcake App - Verify the Start destination through UI Navigation Testing

--- app/src/androidTest/java/io/github/fatimazza/mycomposeapp/cupcake/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/io/github/fatimazza/mycomposeapp/cupcake/CupcakeScreenNavigationTest.kt
@@ -1,10 +1,16 @@
 package io.github.fatimazza.mycomposeapp.cupcake
 
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
 import io.github.fatimazza.mycomposeapp.ui.cupcake.CupcakeApp
+import io.github.fatimazza.mycomposeapp.ui.cupcake.CupcakeScreen
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class CupcakeScreenNavigationTest {
     /**
@@ -14,10 +20,20 @@ class CupcakeScreenNavigationTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
+    private lateinit var navController: TestNavHostController
+
     @Before
     fun setupCupcakeNavHost() {
         composeTestRule.setContent {
-            CupcakeApp()
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeApp(navController = navController)
         }
+    }
+
+    @Test
+    fun cupcakeNavHost_verifyStartDestination() {
+        assertEquals(CupcakeScreen.Start.name, navController.currentBackStackEntry?.destination?.route)
     }
 }
