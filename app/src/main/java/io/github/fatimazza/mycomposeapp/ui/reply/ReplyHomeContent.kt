3ef898754f90ae96396af857743aeb6d9258031c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-24 18:18:05 +0700

    Reply App - Add Home Top Bar containing Logo and Profile Image

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyHomeContent.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyHomeContent.kt
@@ -2,14 +2,20 @@ package io.github.fatimazza.mycomposeapp.ui.reply
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import io.github.fatimazza.mycomposeapp.R
@@ -45,6 +51,28 @@ fun ReplyProfileImage(
             modifier = Modifier.clip(CircleShape),
             painter = painterResource(drawableResource),
             contentDescription = description,
+        )
+    }
+}
+
+@Composable
+private fun ReplyHomeTopBar(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        ReplyLogo(
+            modifier = Modifier
+                .size(dimensionResource(R.dimen.topbar_logo_size))
+                .padding(start = dimensionResource(R.dimen.topbar_logo_padding_start))
+        )
+        ReplyProfileImage(
+            drawableResource = R.drawable.reply_avatar_1,
+            description = stringResource(R.string.email_profile),
+            modifier = Modifier
+                .padding(end = dimensionResource(R.dimen.topbar_profile_image_padding_end))
+                .size(dimensionResource(R.dimen.topbar_profile_image_size))
         )
     }
 }
