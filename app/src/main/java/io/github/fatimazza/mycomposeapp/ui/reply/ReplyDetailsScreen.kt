07b32059dc8c426f6741ef77c10f7f1a6447fa1b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-24 19:48:37 +0700

    Reply App - Add Detail Screen Top Bar composable

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyDetailsScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyDetailsScreen.kt
@@ -1,8 +1,25 @@
 package io.github.fatimazza.mycomposeapp.ui.reply
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
 
 @Composable
 fun ReplyDetailsScreen(
@@ -13,3 +30,42 @@ fun ReplyDetailsScreen(
     }
 }
 
+@Composable
+private fun ReplyDetailsScreenTopBar(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(
+            onClick = { },
+            modifier = Modifier
+                .padding(horizontal = dimensionResource(R.dimen.detail_topbar_back_button_padding_horizontal))
+                .background(MaterialTheme.colorScheme.surface, shape = CircleShape),
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = stringResource(id = R.string.navigation_back)
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = dimensionResource(R.dimen.detail_subject_padding_end))
+        ) {
+            Text(
+                text = stringResource(R.string.email_1_subject),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ReplyDetailsScreenTopBarPreview() {
+    ReplyDetailsScreenTopBar()
+}
