9de864ec7aee6a265f4813e5b896d0d769797d0f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-24 11:42:19 +0700

    Reply App - Add Navigation Item Content containing Mailbox Type

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyHomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyHomeScreen.kt
@@ -1,15 +1,44 @@
 package io.github.fatimazza.mycomposeapp.ui.reply
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Drafts
+import androidx.compose.material.icons.filled.Inbox
+import androidx.compose.material.icons.filled.Report
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.data.reply.MailboxType
 
 @Composable
 fun ReplyHomeScreen(
     modifier: Modifier = Modifier
 ) {
+    val navigationItemContentList = listOf(
+        NavigationItemContent(
+            mailboxType = MailboxType.Inbox,
+            icon = Icons.Default.Inbox,
+            text = stringResource(id = R.string.email_tab_inbox)
+        ),
+        NavigationItemContent(
+            mailboxType = MailboxType.Sent,
+            icon = Icons.Default.Send,
+            text = stringResource(id = R.string.email_tab_sent)
+        ),
+        NavigationItemContent(
+            mailboxType = MailboxType.Drafts,
+            icon = Icons.Default.Drafts,
+            text = stringResource(id = R.string.email_tab_drafts)
+        ),
+        NavigationItemContent(
+            mailboxType = MailboxType.Spam,
+            icon = Icons.Default.Report,
+            text = stringResource(id = R.string.email_tab_spam)
+        )
+    )
     ReplyAppContent(modifier)
 }
 
