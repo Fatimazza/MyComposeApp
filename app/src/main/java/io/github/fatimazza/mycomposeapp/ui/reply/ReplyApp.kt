d6494cd5b7a21ee3d538ef0028ba5d718321fb71
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 06:57:41 +0700

    Reply App - Prepare Navigation by Type

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyApp.kt
@@ -7,23 +7,32 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.fatimazza.mycomposeapp.data.reply.Email
 import io.github.fatimazza.mycomposeapp.data.reply.MailboxType
+import io.github.fatimazza.mycomposeapp.ui.reply.utils.ReplyNavigationType
 
 @Composable
 fun ReplyApp(
     windowSize: WindowWidthSizeClass,
-    modifier : Modifier = Modifier
+    modifier: Modifier = Modifier
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
 
+    val navigationType: ReplyNavigationType
     when (windowSize) {
         WindowWidthSizeClass.Compact -> {
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
+
         WindowWidthSizeClass.Medium -> {
+            navigationType = ReplyNavigationType.NAVIGATION_RAIL
         }
+
         WindowWidthSizeClass.Expanded -> {
+            navigationType = ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
         }
+
         else -> {
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
     }
 
