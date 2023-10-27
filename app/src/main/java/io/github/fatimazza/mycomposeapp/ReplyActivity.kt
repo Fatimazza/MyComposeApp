c598c3cac3267eb8ac009c613a8580ab69265401
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 06:45:39 +0700

    Reply App - Implement different screen size adaptation using Window Size Class

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ReplyActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ReplyActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.ui.reply.ReplyApp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class ReplyActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeAppTheme {
                 Surface {
-                    ReplyApp()
+                    val windowSize = calculateWindowSizeClass(this)
+                    ReplyApp(
+                        windowSize = windowSize.widthSizeClass
+                    )
                 }
             }
         }
@@ -27,7 +34,9 @@ class ReplyActivity : ComponentActivity() {
 fun ReplyAppCompatPreview() {
     MyComposeAppTheme {
         Surface {
-            ReplyApp()
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact,
+            )
         }
     }
 }
--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyApp.kt
@@ -1,5 +1,6 @@
 package io.github.fatimazza.mycomposeapp.ui.reply
 
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -9,10 +10,22 @@ import io.github.fatimazza.mycomposeapp.data.reply.MailboxType
 
 @Composable
 fun ReplyApp(
+    windowSize: WindowWidthSizeClass,
     modifier : Modifier = Modifier
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
+
+    when (windowSize) {
+        WindowWidthSizeClass.Compact -> {
+        }
+        WindowWidthSizeClass.Medium -> {
+        }
+        WindowWidthSizeClass.Expanded -> {
+        }
+        else -> {
+        }
+    }
 
     ReplyHomeScreen(
         replyUiState = replyUiState,
