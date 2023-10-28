0dd9b9035f373722a61e3b75948fd9c2b68c5c2f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 05:40:19 +0700

    Mars Photos App - Add Scaffold and Surface

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsPhotoApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsPhotoApp.kt
@@ -1,10 +1,32 @@
 package io.github.fatimazza.mycomposeapp.ui.mars
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MarsPhotosApp() {
+    Scaffold(
+        modifier = Modifier,
+        topBar = { MarsTopAppBar() }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun MarsTopAppBar() {
+
 }
 
 @Preview(showBackground = true)
