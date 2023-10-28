4d448b3cc6658e6f3395503b92e7dc6f283694bd
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 08:25:50 +0700

    Reply App - Prepare Content Type by Screen Size

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/ReplyApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.fatimazza.mycomposeapp.data.reply.Email
 import io.github.fatimazza.mycomposeapp.data.reply.MailboxType
+import io.github.fatimazza.mycomposeapp.ui.reply.utils.ReplyContentType
 import io.github.fatimazza.mycomposeapp.ui.reply.utils.ReplyNavigationType
 
 @Composable
@@ -18,21 +19,27 @@ fun ReplyApp(
     val replyUiState = viewModel.uiState.collectAsState().value
 
     val navigationType: ReplyNavigationType
+    val contentType: ReplyContentType
+
     when (windowSize) {
         WindowWidthSizeClass.Compact -> {
             navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+            contentType = ReplyContentType.LIST_ONLY
         }
 
         WindowWidthSizeClass.Medium -> {
             navigationType = ReplyNavigationType.NAVIGATION_RAIL
+            contentType = ReplyContentType.LIST_ONLY
         }
 
         WindowWidthSizeClass.Expanded -> {
             navigationType = ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
+            contentType = ReplyContentType.LIST_AND_DETAIL
         }
 
         else -> {
             navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+            contentType = ReplyContentType.LIST_ONLY
         }
     }
 
--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/utils/WindowStateUtils.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/reply/utils/WindowStateUtils.kt
@@ -3,3 +3,8 @@ package io.github.fatimazza.mycomposeapp.ui.reply.utils
 enum class ReplyNavigationType {
     BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
 }
+
+enum class ReplyContentType {
+    LIST_ONLY, LIST_AND_DETAIL
+}
+
