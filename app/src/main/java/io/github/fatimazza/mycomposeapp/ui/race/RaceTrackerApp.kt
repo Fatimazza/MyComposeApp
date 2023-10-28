befefb5e261dd1d89f46c6823eee116a6023211c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 14:16:55 +0700

    Race Tracker App - Add Status Indicator containing participant and progress Texts

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
@@ -2,8 +2,14 @@ package io.github.fatimazza.mycomposeapp.ui.race
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
@@ -46,6 +53,42 @@ private fun RaceTrackerScreen(
                 contentDescription = null,
                 modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium)),
             )
+            StatusIndicator()
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+            StatusIndicator()
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+        }
+    }
+}
+
+@Composable
+private fun StatusIndicator(
+    modifier: Modifier = Modifier
+) {
+    Row {
+        Text("participant", Modifier.padding(end = dimensionResource(R.dimen.padding_small)))
+
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(R.string.race_progress_percentage, 0),
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = "100",
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
     }
 }
