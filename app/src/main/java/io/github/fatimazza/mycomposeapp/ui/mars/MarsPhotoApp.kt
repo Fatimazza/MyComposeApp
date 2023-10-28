8f4d976a60c4064c0e12185b8ddac6745d9747f2
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 05:44:05 +0700

    Mars Photos App - Add Top App Bar with scroll behavior

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsPhotoApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsPhotoApp.kt
@@ -2,17 +2,28 @@ package io.github.fatimazza.mycomposeapp.ui.mars
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MarsPhotosApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
-        modifier = Modifier,
-        topBar = { MarsTopAppBar() }
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { MarsTopAppBar(scrollBehavior = scrollBehavior) }
     ) {
         Surface(
             modifier = Modifier
@@ -24,9 +35,19 @@ fun MarsPhotosApp() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MarsTopAppBar() {
-
+fun MarsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.title_mars_photos),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
