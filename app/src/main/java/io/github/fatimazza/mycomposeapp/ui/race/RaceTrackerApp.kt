996264743705186e5d03482fa67bef77dd2f0f6c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 14:20:57 +0700

    Race Tracker App - Add Linear Progress Indicator to show progress on Status Indicator

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -67,13 +71,19 @@ private fun StatusIndicator(
 ) {
     Row {
         Text("participant", Modifier.padding(end = dimensionResource(R.dimen.padding_small)))
-
         Column(
             modifier = modifier
                 .fillMaxWidth()
                 .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
         ) {
+            LinearProgressIndicator(
+                progress = 0.3f,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(dimensionResource(R.dimen.race_progress_indicator_height))
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.race_progress_indicator_corner_radius)))
+            )
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
