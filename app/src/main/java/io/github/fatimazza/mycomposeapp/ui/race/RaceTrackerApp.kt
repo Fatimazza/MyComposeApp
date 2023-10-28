e5b81352598c2f60639bda0edecab7b6edc2ca5f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 14:06:56 +0700

    Race Tracker App - Add Race Tracker screen containing Text

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
@@ -1,12 +1,39 @@
 package io.github.fatimazza.mycomposeapp.ui.race
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 @Composable
 fun RaceTrackerApp() {
+    RaceTrackerScreen()
+}
 
+@Composable
+private fun RaceTrackerScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.race_run),
+            style = MaterialTheme.typography.headlineLarge,
+        )
+    }
 }
 
 @Preview
