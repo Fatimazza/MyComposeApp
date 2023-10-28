a822a42ffd8c17fbce7c16092f47b3eb9be26049
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 14:12:54 +0700

    Race Tracker App - Add Walk icon on Race Tracker screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
@@ -1,14 +1,17 @@
 package io.github.fatimazza.mycomposeapp.ui.race
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.R
@@ -33,6 +36,17 @@ private fun RaceTrackerScreen(
             text = stringResource(R.string.race_run),
             style = MaterialTheme.typography.headlineLarge,
         )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.race_walk),
+                contentDescription = null,
+                modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium)),
+            )
+        }
     }
 }
 
