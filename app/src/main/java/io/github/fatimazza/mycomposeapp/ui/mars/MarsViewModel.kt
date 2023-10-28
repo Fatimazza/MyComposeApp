c192e3abddc013ba1a4a6150433b81846806df97
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 06:07:50 +0700

    Mars Photos App - Add UI State in View Model

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsViewModel.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsViewModel.kt
@@ -1,7 +1,13 @@
 package io.github.fatimazza.mycomposeapp.ui.mars
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MarsViewModel : ViewModel() {
 
+    /** The mutable State that stores the status of the most recent request */
+    var marsUiState: String by mutableStateOf("")
+        private set
 }
