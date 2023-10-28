ded4385dc31566cf9032c361453e85b04bb686e4
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 06:43:52 +0700

    Mars Photos App - Add function to get Mars Photos in View Model

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsViewModel.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsViewModel.kt
@@ -10,4 +10,19 @@ class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
     var marsUiState: String by mutableStateOf("")
         private set
+
+    /**
+     * Call getMarsPhotos() on init so we can display status immediately.
+     */
+    init {
+        getMarsPhotos()
+    }
+
+    /**
+     * Gets Mars photos information from the Mars API Retrofit service and updates the
+     * [MarsPhoto] [List] [MutableList].
+     */
+    fun getMarsPhotos() {
+        marsUiState = "Set the Mars API status response here!"
+    }
 }
