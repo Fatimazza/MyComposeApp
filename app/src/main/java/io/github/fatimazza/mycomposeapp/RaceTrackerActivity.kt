ab65165d71ba649eebfe3611afe39ecbfe2107b3
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-28 13:58:54 +0700

    Race Tracker App - Add empty Screen to replace unused Composable function

--- app/src/main/java/io/github/fatimazza/mycomposeapp/RaceTrackerActivity.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/RaceTrackerActivity.kt
@@ -3,13 +3,7 @@ package io.github.fatimazza.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.ui.race.RaceTrackerApp
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class RaceTrackerActivity : ComponentActivity() {
@@ -17,30 +11,9 @@ class RaceTrackerActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-                }
+                RaceTrackerApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyComposeAppTheme {
-        Greeting2("Android")
-    }
-}
---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/race/RaceTrackerApp.kt
@@ -0,0 +1,18 @@
+package io.github.fatimazza.mycomposeapp.ui.race
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
+
+@Composable
+fun RaceTrackerApp() {
+
+}
+
+@Preview
+@Composable
+fun RaceTrackerAppPreview() {
+    MyComposeAppTheme {
+        RaceTrackerApp()
+    }
+}
