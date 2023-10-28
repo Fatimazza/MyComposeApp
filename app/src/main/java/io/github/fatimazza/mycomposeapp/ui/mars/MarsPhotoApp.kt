7ecf32bc72630806488171f59391981f355dac42
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 03:40:34 +0700

    Mars Photos App - Add empty Screen to replace unused Composable function

---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsPhotoApp.kt
@@ -0,0 +1,15 @@
+package io.github.fatimazza.mycomposeapp.ui.mars
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MarsPhotosApp() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ResultScreenPreview() {
+    fun MarsPhotosApp() {
+    }
+}
