cdd43035c6a39b51ed2a85607d6bbcb61e416fa9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 11:44:03 +0700

    Mars Photos App - Update API Service to use Kotlin Serialization

--- app/src/main/java/io/github/fatimazza/mycomposeapp/network/MarsApiService.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/network/MarsApiService.kt
@@ -1,7 +1,9 @@
 package io.github.fatimazza.mycomposeapp.network
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL =
@@ -11,7 +13,7 @@ private const val BASE_URL =
  * Use the Retrofit builder to build a retrofit object
  */
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
 
