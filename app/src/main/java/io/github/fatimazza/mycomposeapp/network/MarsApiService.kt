3934eac030be80d7c48ed914dffec02dfc97c86b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 11:46:54 +0700

    Mars Photos App - Update get photo function to return List instead of String in Mars API Service

--- app/src/main/java/io/github/fatimazza/mycomposeapp/network/MarsApiService.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/network/MarsApiService.kt
@@ -1,6 +1,7 @@
 package io.github.fatimazza.mycomposeapp.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import io.github.fatimazza.mycomposeapp.model.MarsPhoto
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
@@ -22,7 +23,7 @@ private val retrofit = Retrofit.Builder()
  */
 interface MarsApiService {
     @GET("photos")
-    suspend fun getPhotos(): String
+    suspend fun getPhotos(): List<MarsPhoto>
 }
 
 /**
