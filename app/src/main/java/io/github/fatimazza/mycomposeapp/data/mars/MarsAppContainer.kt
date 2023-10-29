40cafb3d8f0542e06bd9109bd5391ae3ff00c20d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 14:03:06 +0700

    Mars Photos App v2 - Move Retrofit object and its initialization in App Container class

--- app/src/main/java/io/github/fatimazza/mycomposeapp/data/mars/MarsAppContainer.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/data/mars/MarsAppContainer.kt
@@ -1,5 +1,11 @@
 package io.github.fatimazza.mycomposeapp.data.mars
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import io.github.fatimazza.mycomposeapp.network.MarsApiService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
 /**
  * Dependency Injection container at the application level.
  */
@@ -13,6 +19,23 @@ interface MarsAppContainer {
  * Variables are initialized lazily and the same instance is shared across the whole app.
  */
 class DefaultAppContainer : MarsAppContainer {
+    private val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+    /**
+     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+     */
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    /**
+     * Retrofit service object for creating api calls
+     */
+    private val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+    
     override val marsPhotosRepository: MarsPhotosRepository
         get() = TODO("Not yet implemented")
 
