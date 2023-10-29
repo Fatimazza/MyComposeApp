41c4b46fe4f50b70845834171ed3c804207920ec
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 11:20:49 +0700

    Mars Photos App - Show Loading Screen when UI State is Loading

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsHomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsHomeScreen.kt
@@ -1,12 +1,17 @@
 package io.github.fatimazza.mycomposeapp.ui.mars
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -16,11 +21,28 @@ fun MarsHomeScreen(
     modifier: Modifier = Modifier
 ) {
     when (marsUiState) {
+        is MarsUiState.Loading -> LoadingScreen(
+            modifier = modifier.fillMaxSize()
+        )
+
         is MarsUiState.Success -> ResultScreen(
             marsUiState.photos, modifier
         )
+
         else -> {}
     }
+}
+
+/**
+ * The home screen displaying the loading message.
+ */
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.mars_loading_image),
+        contentDescription = stringResource(R.string.mars_loading)
+    )
 }
 
 /**
