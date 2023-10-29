8424d4c55dc647a806bb72f42a4d96ce2b90bd3a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-29 13:44:30 +0700

    Mars Photos App v2 - Add Mars Photos Repository class under Data package

---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/data/mars/MarsPhotosRepository.kt
@@ -0,0 +1,20 @@
+package io.github.fatimazza.mycomposeapp.data.mars
+
+import io.github.fatimazza.mycomposeapp.model.MarsPhoto
+import io.github.fatimazza.mycomposeapp.network.MarsApi
+
+/**
+ * Repository that fetch mars photos list from marsApi.
+ */
+interface MarsPhotosRepository {
+    suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+/**
+ * Network Implementation of Repository that fetch mars photos list from marsApi.
+ */
+class NetworkMarsPhotosRepository() : MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> {
+        return MarsApi.retrofitService.getPhotos()
+    }
+}
