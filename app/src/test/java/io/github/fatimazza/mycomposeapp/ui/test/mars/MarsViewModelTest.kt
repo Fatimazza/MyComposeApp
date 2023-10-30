4d68f96c08edef0afa3c4f89cab3be672f3f9848
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-30 10:21:44 +0700

    Mars Photos App v2 - Fix View Model Unit Test

--- app/src/test/java/io/github/fatimazza/mycomposeapp/ui/test/mars/MarsViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/mycomposeapp/ui/test/mars/MarsViewModelTest.kt
@@ -2,13 +2,18 @@ package io.github.fatimazza.mycomposeapp.ui.test.mars
 
 import io.github.fatimazza.mycomposeapp.ui.mars.MarsUiState
 import io.github.fatimazza.mycomposeapp.ui.mars.MarsViewModel
+import io.github.fatimazza.mycomposeapp.ui.test.TestDispatcherRule
 import io.github.fatimazza.mycomposeapp.ui.test.mars.fake.MarsFakeDataSource
 import io.github.fatimazza.mycomposeapp.ui.test.mars.fake.MarsFakeNetworkPhotosRepository
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 class MarsViewModelTest {
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
 
     @Test
     fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() =
@@ -17,8 +22,10 @@ class MarsViewModelTest {
                 marsPhotosRepository = MarsFakeNetworkPhotosRepository()
             )
             assertEquals(
-                MarsUiState.Success("Success: ${MarsFakeDataSource.photosList.size} Mars " +
-                        "photos retrieved"),
+                MarsUiState.Success(
+                    "Success: ${MarsFakeDataSource.photosList.size} Mars " +
+                            "photos retrieved"
+                ),
                 marsViewModel.marsUiState
             )
         }
