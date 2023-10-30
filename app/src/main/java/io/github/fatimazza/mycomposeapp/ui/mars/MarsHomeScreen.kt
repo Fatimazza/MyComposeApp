34de22899a0041fde6acd1dcef11027fa12b35db
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-30 16:01:45 +0700

    Mars Photos App v3 - Add Photo Card containing Async Image to display a single photo

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsHomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/mars/MarsHomeScreen.kt
@@ -12,11 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import io.github.fatimazza.mycomposeapp.R
+import io.github.fatimazza.mycomposeapp.model.MarsPhoto
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
 @Composable
@@ -49,6 +53,18 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
         modifier = modifier.size(200.dp),
         painter = painterResource(R.drawable.mars_loading_image),
         contentDescription = stringResource(R.string.mars_loading)
+    )
+}
+
+@Composable
+fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
+    AsyncImage(
+        model = ImageRequest.Builder(context = LocalContext.current)
+            .data(photo.imgSrc)
+            .crossfade(true)
+            .build(),
+        contentDescription = stringResource(R.string.mars_photo),
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
