d39a88bf55f48d2451067ab8ad9f66b52ae01a2c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-30 10:09:11 +0700

    Mars Photos App v2 - Change code to be able Test Coroutine in Repository Unit Test

--- app/src/test/java/io/github/fatimazza/mycomposeapp/ui/test/mars/NetworkMarsRepositoryTest.kt
+++ app/src/test/java/io/github/fatimazza/mycomposeapp/ui/test/mars/NetworkMarsRepositoryTest.kt
@@ -3,16 +3,18 @@ package io.github.fatimazza.mycomposeapp.ui.test.mars
 import io.github.fatimazza.mycomposeapp.data.mars.NetworkMarsPhotosRepository
 import io.github.fatimazza.mycomposeapp.ui.test.mars.fake.MarsFakeApiService
 import io.github.fatimazza.mycomposeapp.ui.test.mars.fake.MarsFakeDataSource
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class NetworkMarsRepositoryTest {
 
     @Test
-    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() {
-        val repository = NetworkMarsPhotosRepository(
-            marsApiService = MarsFakeApiService()
-        )
-        assertEquals(MarsFakeDataSource.photosList, repository.getMarsPhotos())
-    }
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotosRepository(
+                marsApiService = MarsFakeApiService()
+            )
+            assertEquals(MarsFakeDataSource.photosList, repository.getMarsPhotos())
+        }
 }
