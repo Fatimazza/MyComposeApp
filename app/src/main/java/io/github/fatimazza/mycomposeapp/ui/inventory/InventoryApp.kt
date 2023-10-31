1483aa0ae261bd5f349a5ff6795089f5da36688f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-10-31 16:00:58 +0700

    Inventory App - Add Inventory Top App Bar

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/InventoryApp.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/InventoryApp.kt
@@ -1,8 +1,22 @@
 package io.github.fatimazza.mycomposeapp.ui.inventory
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InventoryTopAppBar() {
-
+fun InventoryTopAppBar(
+    title: String = "Inventory App",
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior
+    )
 }
