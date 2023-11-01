a75038f0f1aadf30729dcd7f9120ca08bec64744
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 16:58:39 +0700

    Inventory App - Add Inventory Item Details View Model

---
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemDetailsViewModel.kt
@@ -0,0 +1,18 @@
+package io.github.fatimazza.mycomposeapp.ui.inventory.item
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
+ */
+class ItemDetailsViewModel(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val itemId: Int = checkNotNull(savedStateHandle[InventoryItemDetailsDestination.itemIdArg])
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
