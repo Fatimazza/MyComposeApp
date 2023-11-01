123fbc2deded1ee58134d6360549647873f1f7b5
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 14:01:37 +0700

    Inventory App - Add Floating Action Button in Item Detail Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemDetailsScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemDetailsScreen.kt
@@ -1,10 +1,17 @@
 package io.github.fatimazza.mycomposeapp.ui.inventory.item
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.R
@@ -32,7 +39,21 @@ fun InventoryItemDetailsScreen(
                 canNavigateBack = true,
                 navigateUp = navigateBack
             )
-        }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.inventory_edit_item_title),
+                )
+            }
+        },
+        modifier = modifier
     ) {
 
     }
