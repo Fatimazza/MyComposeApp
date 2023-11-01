c0af52342a6c80fe6199636da79cd48ac92b3b68
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 21:08:26 +0700

    Inventory App - Provide Database management function in Repository

--- app/src/main/java/io/github/fatimazza/mycomposeapp/data/inventory/InventoryItemsRepository.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/data/inventory/InventoryItemsRepository.kt
@@ -1,6 +1,33 @@
 package io.github.fatimazza.mycomposeapp.data.inventory
 
+import kotlinx.coroutines.flow.Flow
+
 /**
  * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
  */
-interface InventoryItemsRepository
+interface InventoryItemsRepository {
+    /**
+     * Retrieve all the items from the the given data source.
+     */
+    fun getAllItemsStream(): Flow<List<InventoryItem>>
+
+    /**
+     * Retrieve an item from the given data source that matches with the [id].
+     */
+    fun getItemStream(id: Int): Flow<InventoryItem?>
+
+    /**
+     * Insert item in the data source
+     */
+    suspend fun insertItem(item: InventoryItem)
+
+    /**
+     * Delete item from the data source
+     */
+    suspend fun deleteItem(item: InventoryItem)
+
+    /**
+     * Update item in the data source
+     */
+    suspend fun updateItem(item: InventoryItem)
+}
--- app/src/main/java/io/github/fatimazza/mycomposeapp/data/inventory/InventoryOfflineItemsRepository.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/data/inventory/InventoryOfflineItemsRepository.kt
@@ -1,3 +1,25 @@
 package io.github.fatimazza.mycomposeapp.data.inventory
 
-class InventoryOfflineItemsRepository : InventoryItemsRepository
+import kotlinx.coroutines.flow.Flow
+
+class InventoryOfflineItemsRepository : InventoryItemsRepository {
+    override fun getAllItemsStream(): Flow<List<InventoryItem>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemStream(id: Int): Flow<InventoryItem?> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertItem(item: InventoryItem) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteItem(item: InventoryItem) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateItem(item: InventoryItem) {
+        TODO("Not yet implemented")
+    }
+}
