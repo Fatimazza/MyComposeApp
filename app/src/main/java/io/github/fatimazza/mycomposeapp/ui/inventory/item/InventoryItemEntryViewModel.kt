a0929cb7b0275f3bac441a1ec5598276f0e5e9ea
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 17:54:56 +0700

    Inventory App - Add some extension functions in Inventory Item Entry View Model

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemEntryViewModel.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemEntryViewModel.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import io.github.fatimazza.mycomposeapp.data.inventory.InventoryItem
+import java.text.NumberFormat
 
 /**
  * ViewModel to validate and insert items in the Room database.
@@ -21,7 +23,10 @@ class InventoryItemEntryViewModel : ViewModel() {
      */
     fun updateUiState(itemDetails: InventoryItemDetails) {
         itemUiState =
-            InventoryItemUiState(itemDetails = itemDetails, isEntryValid = validateInput(itemDetails))
+            InventoryItemUiState(
+                itemDetails = itemDetails,
+                isEntryValid = validateInput(itemDetails)
+            )
     }
 
     private fun validateInput(uiState: InventoryItemDetails = itemUiState.itemDetails): Boolean {
@@ -44,4 +49,39 @@ data class InventoryItemDetails(
     val name: String = "",
     val price: String = "",
     val quantity: String = "",
+)
+
+/**
+ * Extension function to convert [ItemDetails] to [Item]. If the value of [ItemDetails.price] is
+ * not a valid [Double], then the price will be set to 0.0. Similarly if the value of
+ * [ItemDetails.quantity] is not a valid [Int], then the quantity will be set to 0
+ */
+fun InventoryItemDetails.toItem(): InventoryItem = InventoryItem(
+    id = id,
+    name = name,
+    price = price.toDoubleOrNull() ?: 0.0,
+    quantity = quantity.toIntOrNull() ?: 0
+)
+
+fun InventoryItem.formatedPrice(): String {
+    return NumberFormat.getCurrencyInstance().format(price)
+}
+
+/**
+ * Extension function to convert [Item] to [ItemUiState]
+ */
+fun InventoryItem.toItemUiState(isEntryValid: Boolean = false): InventoryItemUiState =
+    InventoryItemUiState(
+        itemDetails = this.toItemDetails(),
+        isEntryValid = isEntryValid
+    )
+
+/**
+ * Extension function to convert [Item] to [ItemDetails]
+ */
+fun InventoryItem.toItemDetails(): InventoryItemDetails = InventoryItemDetails(
+    id = id,
+    name = name,
+    price = price.toString(),
+    quantity = quantity.toString()
 )
