b8fe7c8ae6327b5d26f26f64cbc80f9e9a29cb3a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 12:24:25 +0700

    Inventory App - Add Floating Action Button in Home Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/home/InventoryHomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/home/InventoryHomeScreen.kt
@@ -1,11 +1,18 @@
 package io.github.fatimazza.mycomposeapp.ui.inventory.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.fatimazza.mycomposeapp.R
@@ -34,7 +41,20 @@ fun InventoryHomeScreen(
                 canNavigateBack = false,
                 scrollBehavior = scrollBehavior
             )
-        }) {
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.inventory_item_entry_title)
+                )
+            }
+        },
+    ) {
     }
 }
 
