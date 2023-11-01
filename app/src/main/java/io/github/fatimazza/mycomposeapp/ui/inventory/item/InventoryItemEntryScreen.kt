5bd7dcc309c26c9d46d15063504c493c4061dec0
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 17:34:37 +0700

    Inventory App - Add Item Entry Body composable in Item Entry Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemEntryScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemEntryScreen.kt
@@ -1,9 +1,21 @@
 package io.github.fatimazza.mycomposeapp.ui.inventory.item
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.inventory.InventoryTopAppBar
 import io.github.fatimazza.mycomposeapp.ui.inventory.navigation.InventoryNavDestination
@@ -29,7 +41,30 @@ fun InventoryItemEntryScreen(
                 navigateUp = onNavigateUp
             )
         }
-    ) {
+    ) { innerPadding ->
+        InventoryItemEntryBody(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
+@Composable
+fun InventoryItemEntryBody(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        Button(
+            onClick = {},
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(R.string.inventory_save_action))
+        }
     }
 }
