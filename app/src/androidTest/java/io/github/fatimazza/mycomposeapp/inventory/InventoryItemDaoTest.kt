f650b8520a13803b26773ca8af4045c48e11a565
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-02 06:31:13 +0700

    Inventory App - Test Insert to Database in Inventory Item Dao Test

--- app/src/androidTest/java/io/github/fatimazza/mycomposeapp/inventory/InventoryItemDaoTest.kt
+++ app/src/androidTest/java/io/github/fatimazza/mycomposeapp/inventory/InventoryItemDaoTest.kt
@@ -5,15 +5,23 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.github.fatimazza.mycomposeapp.data.inventory.InventoryDatabase
+import io.github.fatimazza.mycomposeapp.data.inventory.InventoryItem
 import io.github.fatimazza.mycomposeapp.data.inventory.InventoryItemDao
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class InventoryItemDaoTest {
 
     private lateinit var itemDao: InventoryItemDao
     private lateinit var inventoryDatabase: InventoryDatabase
+    private val item1 = InventoryItem(1, "Apples", 10.0, 20)
 
     @Before
     fun createDb() {
@@ -26,4 +34,23 @@ class InventoryItemDaoTest {
             .build()
         itemDao = inventoryDatabase.itemDao()
     }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        inventoryDatabase.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoInsert_insertsItemIntoDB() = runBlocking {
+        addOneItemToDb()
+        val allItems = itemDao.getAllItems().first()
+        assertEquals(allItems[0], item1)
+    }
+
+    private suspend fun addOneItemToDb() {
+        itemDao.insert(item1)
+    }
+
 }
