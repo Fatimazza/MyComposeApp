6793cc927e37c903f52ed31694534ece3b664b8b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 16:48:41 +0700

    Inventory App - Add Delete Inventory Confirmation Dialog

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemDetailsScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/inventory/item/InventoryItemDetailsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -22,6 +23,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -163,6 +165,37 @@ private fun InventoryItemDetailsRow(
         Spacer(modifier = Modifier.weight(1f))
         Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
+}
+
+@Composable
+private fun InventoryDeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { },
+        title = { Text(stringResource(R.string.inventory_attention)) },
+        text = { Text(stringResource(R.string.inventory_delete_question)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(stringResource(R.string.inventory_no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.inventory_yes))
+            }
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InventoryDeleteConfirmationDialogPreview() {
+    InventoryDeleteConfirmationDialog(
+        onDeleteCancel = {},
+        onDeleteConfirm = {},
+    )
 }
 
 @Preview(showBackground = true)
