871e7083e681e807db8330e38c98d80b1ea01ac8
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-01 20:53:53 +0700

    Inventory App - Make Inventory Item as Database Entity

--- app/src/main/java/io/github/fatimazza/mycomposeapp/data/inventory/InventoryItem.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/data/inventory/InventoryItem.kt
@@ -1,9 +1,14 @@
 package io.github.fatimazza.mycomposeapp.data.inventory
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * Entity data class represents a single row in the database.
  */
+@Entity(tableName = "items")
 class InventoryItem(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
