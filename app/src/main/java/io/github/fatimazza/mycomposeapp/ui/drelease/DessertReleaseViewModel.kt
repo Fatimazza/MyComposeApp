9f5de87e123aac8c1c8b7ff300bdf5b265737be7
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-02 17:04:57 +0700

    Dessert Release App - Add Ui State and Select Layout function in View Model

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/drelease/DessertReleaseViewModel.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/drelease/DessertReleaseViewModel.kt
@@ -4,11 +4,28 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import io.github.fatimazza.mycomposeapp.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 /*
  * View model of Dessert Release components
  */
 class DessertReleaseViewModel() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(DessertReleaseUiState())
+
+    // UI states access for various [DessertReleaseUiState]
+    val uiState: StateFlow<DessertReleaseUiState> = _uiState
+
+    /*
+     * [selectLayout] change the layout and icons accordingly and
+     * save the selection in DataStore through [userPreferencesRepository]
+     */
+    fun selectLayout(isLinearLayout: Boolean) {
+        _uiState.value = DessertReleaseUiState(isLinearLayout)
+    }
+    
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
@@ -17,3 +34,14 @@ class DessertReleaseViewModel() : ViewModel() {
         }
     }
 }
+
+/*
+ * Data class containing various UI States for Dessert Release screens
+ */
+data class DessertReleaseUiState(
+    val isLinearLayout: Boolean = true,
+    val toggleContentDescription: Int =
+        if (isLinearLayout) R.string.release_grid_layout_toggle else R.string.release_linear_layout_toggle,
+    val toggleIcon: Int =
+        if (isLinearLayout) R.drawable.ic_release_grid_layout else R.drawable.ic_release_linear_layout
+)
