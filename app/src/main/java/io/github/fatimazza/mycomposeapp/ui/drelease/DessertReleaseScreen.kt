c8008d6c210232ac1a08d0a36ac696d2f4a123c0
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-02 16:36:44 +0700

    Dessert Release App - Add empty Screen to replace unused Composables

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/drelease/DessertReleaseScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/drelease/DessertReleaseScreen.kt
@@ -1,4 +1,26 @@
 package io.github.fatimazza.mycomposeapp.ui.drelease
 
-class DessertReleaseScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
+
+/*
+ * Screen level composable
+ */
+@Composable
+fun DessertReleaseApp() {
+
+}
+
+@Composable
+private fun DessertReleaseScreen() {
+
+}
+
+@Preview
+@Composable
+fun DessertReleaseAppPreview() {
+    MyComposeAppTheme {
+        DessertReleaseScreen()
+    }
 }
