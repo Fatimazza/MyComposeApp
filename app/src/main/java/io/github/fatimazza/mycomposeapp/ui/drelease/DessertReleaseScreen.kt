07b5701010170d278ccf5db8a793630b0eea56ec
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-02 16:51:29 +0700

    Dessert Release App - Prepare Linear Layout in Dessert Release Screen

--- app/src/main/java/io/github/fatimazza/mycomposeapp/ui/drelease/DessertReleaseScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycomposeapp/ui/drelease/DessertReleaseScreen.kt
@@ -1,6 +1,13 @@
 package io.github.fatimazza.mycomposeapp.ui.drelease
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -10,9 +17,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycomposeapp.R
 import io.github.fatimazza.mycomposeapp.ui.theme.MyComposeAppTheme
 
@@ -48,8 +59,46 @@ private fun DessertReleaseScreen() {
                 )
             )
         }
-    ) {
+    ) { innerPadding ->
+        val modifier = Modifier
+            .padding(
+                top = dimensionResource(R.dimen.padding_medium),
+                start = dimensionResource(R.dimen.padding_medium),
+                end = dimensionResource(R.dimen.padding_medium),
+            )
+        DessertReleaseLinearLayout(
+            modifier = modifier.fillMaxWidth(),
+            contentPadding = innerPadding
+        )
+    }
+}
 
+@Composable
+fun DessertReleaseLinearLayout(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+    ) {
+        item {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text(
+                    text = "dessert",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(dimensionResource(R.dimen.padding_medium)),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
 
